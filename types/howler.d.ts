5e9f10a5b840b4ae02c3d8459c8e1961f1131ee6
kamikazenn <mklopghi37@gmail.com>
2025-12-18 18:48:48 +0530

    fix: howler2

---
+++ types/howler.d.ts
@@ -0,0 +1,19 @@
+declare module "howler" {
+  export interface HowlOptions {
+    src: string[];
+    volume?: number;
+    loop?: boolean;
+    preload?: boolean;
+    html5?: boolean;
+    onend?: () => void;
+  }
+
+  export class Howl {
+    constructor(options: HowlOptions);
+    play(): number;
+    stop(): void;
+    pause(): void;
+    unload(): void;
+    volume(volume?: number): number | void;
+  }
+}
